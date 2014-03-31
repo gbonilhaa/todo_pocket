72085fc4ed281fd3f9263a1989c77e9a395edaf7
Gustavo Bonilha <gustavo@iMac20.local>
2014-03-31 11:42:16 -0300

    arrumado edit user,falta alinhar, e ajustando campo para salver usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,11 +1,12 @@
 class UsuariosController < ApplicationController
+
     def create
-        Usuario.create nome: params[:nome], email: params[:email], senha: params[:senha]
+      Usuario.create nome: params[:nome], email: params[:email], password_digest: params[:password_digest]
         redirect_to :back
       end
 
     def update
-        Tarefa.update nome: params[:nome], senha: params[:senha]
+        Tarefa.update nome: params[:nome], password_digest: params[:password_digest]
         redirect_to tarefas_path
       end
   end
