eb311a5b828d528e23e6532fcd8bb1a23a7d5d22
Gustavo Bonilha <gustavo@iMac20.local>
2014-03-31 12:04:00 -0300

    edit usuarios controller

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,27 +1,36 @@
 class UsuariosController < ApplicationController
+    def index
+      @usuarios = Usuario.all
+    end
 
     def create
       Usuario.create usuario_params
-        redirect_to :back
-      end
+      redirect_to usuarios_path
+    end
 
     def update
       Usuario.update params[:id], usuario_params
       redirect_to usuarios_path
     end
 
+    def destroy
+      usuario = Usuario.find params[:id]
+      usuario.destroy
+      redirect_to usuarios_path
+    end
+
     def new
-          @usuario = Usuario.new
+      @usuario = Usuario.new
     end
 
     def edit
-          @usuario = Usuario.find params[:id]
-          render :new
+      @usuario = Usuario.find params[:id]
+      render :new
     end
 
     private
 
     def usuario_params
-          params.require(:usuario).permit(:nome, :email, :password_digest)
+      params.require(:usuario).permit(:nome, :email, :password_digest)
     end
   end
