b39399474801237f73c18b2743cc608e16781aa9
gustavo <gbonilha@paliari.com.br>
2014-04-01 20:08:18 +0000

    salvar o id do cadastro na session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@ class SessionsController < ApplicationController
   def create
     	cadastro = cadastro.find_by_email(params[:email])
     	if cadastro && cadastro.authenticate(params[:password])
+    		session[:id] = cadastro.id
+    		redirect_to root_path
     else
     	render :new
     end
